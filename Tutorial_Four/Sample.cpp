9564c4f6483ca1e343c062650dc15d8cb7a74cb1
abijanakiraman <janakiraman.abinav@gmail.com>
2013-08-24 00:10:01 -0400

    Update Sample.cpp

--- Tutorial_Four/Sample.cpp
+++ Tutorial_Four/Sample.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
 	int temp;
 	cin >> temp;
 	cout << "BASIC CONDITIONAL STATEMENT" << endl;
 	if(temp < 25){
-		cout << "It is too cold << endl;
+		cout << "It is too cold" << endl;
 	}else if(temp > 50){
-		cout << "It is too warm << endl;
+		cout << "It is too warm" << endl;
 	}else{
 		cout << "The temperature is perfect" << endl;
 	}
+	
 	cout << "NESTED CONDITIONAL STATEMENTS" << endl;
 	if(temp >= 25){
 		if(temp <= 50){
@@ -21,14 +24,57 @@ int main(){
 	}else{
 		cout << "The temperature is not perfect" << endl;
 	}
+	
 	cout << "CONDITIONAL STATEMENTS WITH THE AND OPERATOR" << endl;
 	if(temp >= 25 && temp <= 50){
 		cout << "The temperature is perfect" << endl;
 	}else{
 		cout << "The temperature is not perfect" << endl;
 	}
+	
 	cout << "CONDITIONAL STATEMENTS WITH THE OR OPERATOR" << endl;
-	if(temp < 25 || temp < 50){
+	if(temp < 25 || temp > 50){
 		cout << "The temperature is not perfect" << endl;
 	}else{ 
 		cout << "The temperature is perfect" << endl; 
+	}
+	
+	cout << "WHILE LOOP" << endl;
+	if(temp < 25){
+		cout << "It is too cold" << endl;
+		int counter = temp;
+		while(counter < 25){
+			counter++;
+			cout << "The temperature has been increased to " << counter << " degrees" << endl;
+		}
+		cout << "The temperature is now perfect";
+	}else if(temp > 50){
+		cout << "It is too warm" << endl;
+		int counter = temp;
+		while(counter >= 50){
+			counter--;
+			cout << "The temperature has been decreased to " << temp << " degrees" << endl;
+		}
+		cout << "The temperature is now perfect" << endl;
+	}else{
+		cout << "The temperature is perfect" << endl;
+	}
+	cout << "FOR LOOP" << endl;
+	if(temp < 25){
+		cout << "It is too cold" << endl;
+		for(int counter = temp; counter < 25; ++counter){
+			cout << "The temperature has been increased to " << counter << " degrees" << endl;
+		}
+		cout << "The temperature is now perfect";
+	}else if(temp > 50){
+		cout << "It is too warm" << endl;
+		for(int counter = temp; counter >50; --counter){
+			cout << "The temperature has been decreased to " << counter << " degrees" << endl;
+		}
+		cout << "The temperature is now perfect";
+	}else{
+		cout << "The temperature is perfect" << endl;
+	}
+	return 0;
+
+}
