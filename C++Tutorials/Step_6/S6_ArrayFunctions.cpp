08ed01f4d57831a96c7d4d0dfe9df42facea1711
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 14:18:06 -0400

    Update S6_ArrayFunctions.cpp

--- C++Tutorials/Step_6/S6_ArrayFunctions.cpp
+++ C++Tutorials/Step_6/S6_ArrayFunctions.cpp
@@ -1,3 +1,14 @@
+//
+//  Program Name - S6_ArrayFunctions.cpp
+//  Series: GetOnToC++ Step: 6
+//
+//  Purpose: This program illustrates how to use arrays in functions
+//
+//  Compile: g++  S6_ArrayFunctions.cpp -o  S6_ArrayFunctions
+//  Execute: ./S6_ArrayFunctions
+//
+//  Created by Abinav Janakiraman on 05/10/13.
+//
 #include <iostream>
 using namespace std;
 
