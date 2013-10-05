4ffd04499a192a381032458d091e9b341239b9d2
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 18:01:28 -0400

    Update S6_2DArrays.cpp

--- C++Tutorials/Step_6/S6_2DArrays.cpp
+++ C++Tutorials/Step_6/S6_2DArrays.cpp
@@ -12,21 +12,21 @@
 
 #include <iostream>
 using namespace std;
-
+//Creating a two dimensional array
 int matrix[10][10];
 int main(){
 	//Initializing 2D Array
 	int counter = 0;
-	for(int row = 0; row < 10; row++){
-		for(int col = 0; col < 10; col++){
-			matrix[row][col] = counter++;
+	for(int row = 0; row < 10; row++){ //For loop to traverse the rows
+		for(int col = 0; col < 10; col++){ //For loop to traverse each column of each row
+			matrix[row][col] = counter++; //Setting matrix[row][col] to counter
 		}
 	}
 	
 	//Printing 2D array
-	for(int row = 0; row < 10; row++){
-		for(int col = 0; col < 10; col++){
-			cout << matrix[row][col] << "	";
+	for(int row = 0; row < 10; row++){ //For loop to traverse the rows
+		for(int col = 0; col < 10; col++){ //For loop to traverse each column of each row
+			cout << matrix[row][col] << "	"; //Printing the value of matrix[row][col]
 		}
 		cout << endl;
 	}
