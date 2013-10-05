14cf87ff9e40d4d29ac6a12d07bc6d88e8802bab
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 14:19:05 -0400

    Update S6_TraversingArrays.cpp

--- C++Tutorials/Step_6/S6_TraversingArrays.cpp
+++ C++Tutorials/Step_6/S6_TraversingArrays.cpp
@@ -1,3 +1,14 @@
+//
+//  Program Name - S6_TraversingArrays.cpp
+//  Series: GetOnToC++ Step: 6
+//
+//  Purpose: This program illustrates how to use arrays in functions
+//
+//  Compile: g++  S6_TraversingArrays.cpp -o  S6_TraversingArrays
+//  Execute: ./S6_TraversingArrays
+//
+//  Created by Abinav Janakiraman on 05/10/13.
+//
 #include <iostream>
 using namespace std;
 
