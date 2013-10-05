d7e7b0b18d4a3e4ecf6969b91af8f3c08778a8d0
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 18:23:07 -0400

    Update S6_Arrays.cpp

--- C++Tutorials/Step_6/S6_Arrays.cpp
+++ C++Tutorials/Step_6/S6_Arrays.cpp
@@ -13,8 +13,10 @@
 using namespace std;
 
 const double pi = 3.14159;
+//Cylinder Class
 class cylinder{
 	public:
+		//variables
 		double radius, length;
 		//Default constructor
 		cylinder(){}
@@ -23,13 +25,19 @@ class cylinder{
 };
 
 int main(){
+	//Creating and int rray of size 10
 	int example[10];
+	//Setting the 3 element of the array to two
 	example[3] = 2;
+	//Printing the third element of the array
 	cout << example[3] << endl;
+	//Creating an array of  3 cylinders
 	cylinder oil_tanks[3];
+	//Setting the radius and lenght of each cylinder in the array
 	oil_tanks[0].radius = 5; oil_tanks[0].length = 40.0;
 	oil_tanks[1].radius = 3; oil_tanks[1].length = 60.0;
 	oil_tanks[2].radius = 2; oil_tanks[2].length = 10.0;
+	//Calling the volume function on each cylinder in the array
 	cout << oil_tanks[0].volume() << endl;
 	cout << oil_tanks[1].volume() << endl;
 	cout << oil_tanks[2].volume() << endl;
