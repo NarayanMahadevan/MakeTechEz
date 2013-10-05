d08d3e92a9dc83a2f9a6719c2a0c2fc3b482a911
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 15:23:26 -0400

    Update S7_Pointer_Operations.cpp

--- C++Tutorials/Step_7/S7_Pointer_Operations.cpp
+++ C++Tutorials/Step_7/S7_Pointer_Operations.cpp
@@ -1,3 +1,14 @@
+//
+//  Program Name - S7_Pointer_Operations.cpp
+//  Series: GetOnToC++ Step: 7
+//
+//  Purpose: This program illustrates how to use arithmetic operations on pointers
+//
+//  Compile: g++ S7_Pointer_Operations.cpp -o S7_Pointer_Operations
+//  Execute: ./S7_Pointer_Operations
+//
+//  Created by Narayan Mahadevan on 18/08/13.
+// 
 #include <iostream>
 using namespace std;
 
