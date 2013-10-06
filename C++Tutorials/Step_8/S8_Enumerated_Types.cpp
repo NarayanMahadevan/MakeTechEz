f2eb9fd3ddda08259f2a7a5b2fbccddda1fe754f
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 22:12:08 -0400

    Update S8_Enumerated_Types.cpp

--- C++Tutorials/Step_8/S8_Enumerated_Types.cpp
+++ C++Tutorials/Step_8/S8_Enumerated_Types.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
-
+//Enum where the 4th number has the value of 5
 enum{zero, one, two, three, five = 5, six};
 	
 int main(){
+	//Pringting the different values
 	cout << "The first value is " << zero << endl
 		 << "The second value is " << one << endl
 		 << "The third values is " << two << endl
