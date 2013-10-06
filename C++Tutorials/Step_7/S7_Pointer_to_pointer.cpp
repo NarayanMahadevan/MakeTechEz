d20a374448f16139d6e12ab4ddd9eb8383596b99
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 21:56:20 -0400

    Update S7_Pointer_to_pointer.cpp

--- C++Tutorials/Step_7/S7_Pointer_to_pointer.cpp
+++ C++Tutorials/Step_7/S7_Pointer_to_pointer.cpp
@@ -13,15 +13,20 @@
 using namespace std;
 
 int main(){
+	//Two integers
 	int x = 4;
 	int y = 5;
+	//Two pointers that point to the two integers above
 	int *a = &x;
 	int *b = &y;
+	//A pointer that points to  pointer a
 	int **c = &a;
-	
+	//Printing the original values of *a and *b
 	cout << "*a = " << *a << endl
 		 << "*b = " << *b << endl;
+	//Changing what *c(in other words, a) points to	 
 	*c = &y;
+	//Printing the new values of *a and *b
 	cout << "*a now = " << *a << endl
 		 << "*b now = " << *b << endl;
 	return 0;
