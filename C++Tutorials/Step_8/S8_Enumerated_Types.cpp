b79cf93f2ae3f7967ffa0cedaef97f7bccbdd3f3
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-05 22:32:26 -0400

    Update S8_Enumerated_Types.cpp

--- C++Tutorials/Step_8/S8_Enumerated_Types.cpp
+++ C++Tutorials/Step_8/S8_Enumerated_Types.cpp
@@ -1,3 +1,14 @@
+//
+//  Program Name - S8_Enumerated_Types.cpp
+//  Series: GetOnToC++ Step: 8
+//
+//  Purpose: This program illustrates how to change the values of enumerated types
+//
+//  Compile: g++ S8_Enumerated_Types.cpp -o S8_Enumerated_Types
+//  Execute: ./S8_Enumerated_Types
+//
+//  Created by Narayan Mahadevan on 18/08/13.
+// 
 #include <iostream>
 using namespace std;
 //Enum where the 4th number has the value of 5
