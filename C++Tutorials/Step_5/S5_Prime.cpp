18282e436a7114537c13034ff58d94ca3f26dada
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-12 22:18:51 -0400

    Create S5_Prime.cpp

---
+++ C++Tutorials/Step_5/S5_Prime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int p){
+  int i = 2;
+  if (i==p) return 1;//or better  if (i*i>p) return 1;
+  if (p%i == 0) return 0;
+  return isPrime (p, i+1);
+}
+
+int main(){
+  int p;
+  cout << "Enter a number" << endl;
+  cin >> p;
+  if(isPrime(p) == true){
+    cout << p << " is prime" << endl;
+  }else{
+    cout << p << " is not prime" << endl;
+}
+    
