3ad0700f264a0ec16a4ee1d8e1d3008dd2bf1fc6
abijanakiraman <janakiraman.abinav@gmail.com>
2013-10-19 23:57:58 -0400

    Update and rename S10_primative_reference_functions.cpp to S10_Primative_reference_functions.cpp

--- C++Tutorials/Step_10/S10_Primative_reference_functions.cpp
+++ C++Tutorials/Step_10/S10_Primative_reference_functions.cpp
@@ -1,11 +1,11 @@
 //
-//  Program Name - S10_primative_reference_functions.cpp
+//  Program Name - S10_Primative_reference_functions.cpp
 //  Series: GetOnToC++ Step: 10
 //
 //  Purpose: This program shows that call-by-reference can be used with all types
 //
-//  Compile: g++ S10_primative_reference_functions.cpp -o S10_primative_reference_functions
-//  Execute: ./S10_primative_reference_functions
+//  Compile: g++ S10_Primative_reference_functions.cpp -o S10_Primative_reference_functions
+//  Execute: ./S10_Primative_reference_functions
 //
 //  Created by Narayan Mahadevan on 18/08/13.
 //  
